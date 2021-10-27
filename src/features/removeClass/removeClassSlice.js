1a321e2322e45eddc5eaf011bf4ca13549bf8fdb
Francis Uloko <francisuloko@yahoo.com>
2021-10-27 20:33:17 +0100

    Fix remove classes issue

--- src/features/removeClass/removeClassSlice.js
+++ src/features/removeClass/removeClassSlice.js
@@ -23,6 +23,23 @@ export const removeClassSlice = createSlice({
   name: 'removeClass',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(removeClass.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(removeClass.fulfilled, (state, action) => {
+        if (action.payload.status === 200) {
+          state.status = action.payload.message;
+        } else {
+          state.error = action.payload.message;
+        }
+      })
+      .addCase(removeClass.rejected, (state, action) => {
+        state.error = 'Failed';
+        state.status = action.payload.message;
+      });
+  },
 });
 
 export default removeClassSlice.reducer;
