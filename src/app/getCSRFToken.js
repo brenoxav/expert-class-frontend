18ce91439a4c7d5202f28938c88ea327f13f95bc
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-10-29 02:01:08 -0600

    Test4

--- src/app/getCSRFToken.js
+++ src/app/getCSRFToken.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 export const setSessionCookies = async () => {
   try {
-    await axios.get('https://expert-class-backend.herokuapp.com/', { withCredentials: true });
+    await axios.get('https://expert-class-backend.herokuapp.com/', {
+      withCredentials: false,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    });
     return true;
   } catch (error) {
     return error.message;
