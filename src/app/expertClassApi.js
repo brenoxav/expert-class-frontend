e1a17348c6b2fbd48f8f83d611e0af48126bf43c
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-11-05 16:09:06 -0600

    Create Axios instance to make all API calls

---
+++ src/app/expertClassApi.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const expertClassApi = axios.create({
+  baseURL: 'https://expert-class-backend.herokuapp.com/api/v1/',
+  withCredentials: true,
+});
+
+export default expertClassApi;
--- src/auth/sessionSlice.js
+++ src/auth/sessionSlice.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import expertClassApi from '../app/expertClassApi';
 
 const initialState = {
   user: {},
@@ -12,8 +12,7 @@ const initialState = {
 export const signUpUser = createAsyncThunk(
   'session/signUpUser', async (params, thunkAPI) => {
     try {
-      const response = await axios.post('https://expert-class-backend.herokuapp.com/api/v1/users', { user: params },
-        { withCredentials: true });
+      const response = await expertClassApi.post('users', { user: params });
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue({ error: error.message });
@@ -24,19 +23,8 @@ export const signUpUser = createAsyncThunk(
 export const loginUser = createAsyncThunk(
   'session/loginUser', async (username, thunkAPI) => {
     try {
-      const response = await axios.post('https://expert-class-backend.herokuapp.com/api/v1/sign_in', { user: { username } },
-        { withCredentials: true });
+      const response = await expertClassApi.post('sign_in', { user: { username } });
       return response.data;
-      // const response = await (await fetch('https://expert-class-backend.herokuapp.com/api/v1/sign_in', {
-      //   method: 'POST',
-      //   body: JSON.stringify({ user: { username } }),
-      //   credentials: 'include',
-      //   mode: 'cors',
-      //   headers: {
-      //     accept: 'application/json, text/plain, */*', 'content-type': 'application/json',
-      //   },
-      // })).json();
-      // return response;
     } catch (error) {
       return thunkAPI.rejectWithValue({ error: error.message });
     }
@@ -46,8 +34,7 @@ export const loginUser = createAsyncThunk(
 export const logoutUser = createAsyncThunk(
   'session/logoutUser', async (thunkAPI) => {
     try {
-      const response = await axios.delete('https://expert-class-backend.herokuapp.com/api/v1/sign_out',
-        { withCredentials: true });
+      const response = await expertClassApi.delete('sign_out');
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue({ error: error.message });
@@ -58,17 +45,8 @@ export const logoutUser = createAsyncThunk(
 export const loginStatus = createAsyncThunk(
   'session/loginStatus', async (thunkAPI) => {
     try {
-      const response = await axios.get('https://expert-class-backend.herokuapp.com/api/v1/signed_in', { withCredentials: true });
+      const response = await expertClassApi.get('signed_in');
       return response.data;
-      // const response = await (await fetch('https://expert-class-backend.herokuapp.com/api/v1/signed_in', {
-      //   method: 'GET',
-      //   credentials: 'include',
-      //   mode: 'cors',
-      //   headers: {
-      //     accept: 'application/json, text/plain, */*', 'content-type': 'application/json',
-      //   },
-      // })).json();
-      // return response;
     } catch (error) {
       return thunkAPI.rejectWithValue({ error: error.message });
     }
