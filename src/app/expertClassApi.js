639bc78c1ea01d0ae5474d143f81b4f87f6e0e49
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-11-09 01:04:57 -0600

    Fix bug with images in Carousel not resizing properly when windows resizes

--- src/app/expertClassApi.js
+++ src/app/expertClassApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const expertClassApi = axios.create({
-  // baseURL: 'https://expert-class-backend.herokuapp.com/api/v1/',
   baseURL: 'https://expert-class-backend.herokuapp.com/api/v1/',
+  // baseURL: 'http://localhost:3001/api/v1/',
   withCredentials: true,
 });
 
