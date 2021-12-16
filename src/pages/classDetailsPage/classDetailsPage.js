acd64747bc6ad958067c6490fac36622cdb12200
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-12-16 17:26:32 -0600

    Edit styling of details page

--- src/pages/classDetailsPage/classDetailsPage.js
+++ src/pages/classDetailsPage/classDetailsPage.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { fetchClassDetails } from './classDetailsPageSlice';
-import './classDetailsPage.css';
+import './classDetailsPage.scss';
 
 function ClassDetails() {
   const { id } = useParams();
@@ -15,12 +15,17 @@ function ClassDetails() {
   }, []);
 
   return (
-    <div className="details-container">
+    <div className="private-page-container details-container">
       <div className="details-col-1">
-        <img src={c.course_image_url} alt="expert class" />
+        <div
+          className="class-details-img"
+          style={{
+            background: `url(${c.course_image_url}) center center/cover no-repeat, rgb(153, 140, 140)`,
+          }}
+        />
       </div>
       <div className="details-col-2">
-        <div className="title">
+        <div>
           <h2 className="class-title">{c.title}</h2>
           <h3 className="class-instructor">{c.instructor}</h3>
         </div>
@@ -35,12 +40,12 @@ function ClassDetails() {
               Hour(s)
             </span>
           </h4>
-          <h4>
+          <div>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-megaphone" viewBox="0 0 16 16">
               <path d="M13 2.5a1.5 1.5 0 0 1 3 0v11a1.5 1.5 0 0 1-3 0v-.214c-2.162-1.241-4.49-1.843-6.912-2.083l.405 2.712A1 1 0 0 1 5.51 15.1h-.548a1 1 0 0 1-.916-.599l-1.85-3.49a68.14 68.14 0 0 0-.202-.003A2.014 2.014 0 0 1 0 9V7a2.02 2.02 0 0 1 1.992-2.013 74.663 74.663 0 0 0 2.483-.075c3.043-.154 6.148-.849 8.525-2.199V2.5zm1 0v11a.5.5 0 0 0 1 0v-11a.5.5 0 0 0-1 0zm-1 1.35c-2.344 1.205-5.209 1.842-8 2.033v4.233c.18.01.359.022.537.036 2.568.189 5.093.744 7.463 1.993V3.85zm-9 6.215v-4.13a95.09 95.09 0 0 1-1.992.052A1.02 1.02 0 0 0 1 7v2c0 .55.448 1.002 1.006 1.009A60.49 60.49 0 0 1 4 10.065zm-.657.975 1.609 3.037.01.024h.548l-.002-.014-.443-2.966a68.019 68.019 0 0 0-1.722-.082z" />
             </svg>
             <span>{c.description}</span>
-          </h4>
+          </div>
         </div>
       </div>
     </div>
