dcd7d2af0e5c67bd41fd60dd5c74916f48e995d4
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-12-18 20:34:24 -0600

    Create overlay menu for mobile view

--- src/components/navPanel/navPanel.js
+++ src/components/navPanel/navPanel.js
@@ -18,24 +18,51 @@ function NavPanel() {
   };
 
   return (
-    <nav className="nav-panel">
-      <NavLink to="/classes" className="nav-link" activeClassName="active-nav-link" exact>
-        Classes
-      </NavLink>
-      <NavLink to="/reserve" className="nav-link" activeClassName="active-nav-link" exact>
-        Reserve
-      </NavLink>
-      <NavLink to="/reservations" className="nav-link" activeClassName="active-nav-link" exact>
-        My Reservations
-      </NavLink>
-      <NavLink to="/add-class" className="nav-link" activeClassName="active-nav-link" exact>
-        Add Class
-      </NavLink>
-      <NavLink to="/remove-class" className="nav-link" activeClassName="active-nav-link" exact>
-        Remove Class
-      </NavLink>
-      <button type="button" className="button-white logOutBtn" onClick={handleLogoutClick}>Logout</button>
-    </nav>
+    <>
+      <nav className="nav-panel">
+        <NavLink to="/classes" className="nav-link" activeClassName="active-nav-link" exact>
+          Classes
+        </NavLink>
+        <NavLink to="/reserve" className="nav-link" activeClassName="active-nav-link" exact>
+          Reserve
+        </NavLink>
+        <NavLink to="/reservations" className="nav-link" activeClassName="active-nav-link" exact>
+          My Reservations
+        </NavLink>
+        <NavLink to="/add-class" className="nav-link" activeClassName="active-nav-link" exact>
+          Add Class
+        </NavLink>
+        <NavLink to="/remove-class" className="nav-link" activeClassName="active-nav-link" exact>
+          Remove Class
+        </NavLink>
+        <button type="button" className="button-white logOutBtn" onClick={handleLogoutClick}>Logout</button>
+      </nav>
+      <div className="overlay-nav">
+        <nav className="overlay-nav__nav-list">
+          <NavLink to="/classes" className="nav-link-mobile" activeClassName="" exact>
+            Classes
+          </NavLink>
+          <NavLink to="/reserve" className="nav-link-mobile" activeClassName="" exact>
+            Reserve
+          </NavLink>
+          <NavLink to="/reservations" className="nav-link-mobile" activeClassName="" exact>
+            My Reservations
+          </NavLink>
+          <NavLink to="/add-class" className="nav-link-mobile" activeClassName="" exact>
+            Add Class
+          </NavLink>
+          <NavLink to="/remove-class" className="nav-link-mobile" activeClassName="" exact>
+            Remove Class
+          </NavLink>
+        </nav>
+        <button type="button" className="button-white logOutBtn" onClick={handleLogoutClick}>Logout</button>
+        <p className="overlay-nav__copyright">
+          © Copyright
+          <br />
+          francisuloko, Mihndim2020, brenoxav, and StarSheriff2
+        </p>
+      </div>
+    </>
   );
 }
 
