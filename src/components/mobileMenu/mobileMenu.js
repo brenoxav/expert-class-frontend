073d2d7b2a3aa5acd8691d49fbdf35ce3bd02dbb
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-07 22:51:26 -0600

    refactor mobile menu and nav component

--- src/components/mobileMenu/mobileMenu.js
+++ src/components/mobileMenu/mobileMenu.js
@@ -1,36 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { NavLink, useHistory } from 'react-router-dom';
+import { unwrapResult } from '@reduxjs/toolkit';
+import { logoutUser } from '../../auth/sessionSlice';
 import styles from './mobileMenu.module.scss';
 
 const MobileMenu = () => {
-  const mobileNav = document.querySelector('.overlay-nav');
-  const mobileNavLinks = document.querySelector('.overlay-nav__nav-list');
-  const copyright = document.querySelector('.overlay-nav__copyright');
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const [open, setOpen] = useState(false);
   const handleClick = () => {
     setOpen(!open);
   };
 
-  useEffect(() => {
-    if (open) {
-      mobileNav.classList.add('overlay-nav--show');
-      mobileNavLinks.classList.add('overlay-nav__nav-list--overlay-transition');
-      copyright.classList.add('overlay-nav__copyright--show');
-    } else if (!open && mobileNav) {
-      mobileNav.classList.remove('overlay-nav--show');
-      mobileNavLinks.classList.remove('overlay-nav__nav-list--overlay-transition');
-      copyright.classList.remove('overlay-nav__copyright--show');
+  const handleLogoutClick = async () => {
+    const resultAction = await dispatch(logoutUser());
+    const originalPromiseResult = unwrapResult(resultAction);
+    if (originalPromiseResult.logged_out) {
+      history.replace('/');
     }
-  }, [open]);
+  };
 
   return (
-    <button
-      type="button"
-      className={styles.mobileMenu}
-      onClick={handleClick}
-    >
-      {open ? <i className={`far fa-times ${styles.menuIcon}`} /> : <i className={`fas fa-bars ${styles.menuIcon}`} />}
-    </button>
+    <>
+      <button
+        type="button"
+        className={styles.mobileMenu}
+        onClick={handleClick}
+      >
+        {open ? <i className={`far fa-times ${styles.menuIcon}`} /> : <i className={`fas fa-bars ${styles.menuIcon}`} />}
+      </button>
+
+      {/* Nav Links */}
+      <div className={open ? `${styles.overlayNav} ${styles.overlayNav__show}` : styles.overlayNav}>
+        <nav className={open ? `${styles.overlayNav__navList} ${styles.overlayNav__navList__overlayTransition}` : styles.overlayNav__navList}>
+          <NavLink to="/classes" className={styles.navLinkMobile} activeClassName="" exact>
+            Classes
+          </NavLink>
+          <NavLink to="/reserve" className={styles.navLinkMobile} activeClassName="" exact>
+            Reserve
+          </NavLink>
+          <NavLink to="/reservations" className={styles.navLinkMobile} activeClassName="" exact>
+            My Reservations
+          </NavLink>
+          <NavLink to="/add-class" className={styles.navLinkMobile} activeClassName="" exact>
+            Add Class
+          </NavLink>
+          <NavLink to="/remove-class" className={styles.navLinkMobile} activeClassName="" exact>
+            Remove Class
+          </NavLink>
+        </nav>
+        <button type="button" className="button-white logOutBtn" onClick={handleLogoutClick}>Logout</button>
+        <p className={open ? `${styles.overlayNav__copyright} ${styles.overlayNav__copyright__show}` : styles.overlayNav__copyright}>
+          © Copyright
+          <br />
+          francisuloko, Mihndim2020, brenoxav, and StarSheriff2
+        </p>
+      </div>
+    </>
   );
 };
 
